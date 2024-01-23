aa6c03c731f26133b643163753f02932c1f841eb
Pernilla Sterner <psterner@hotmail.com>
2024-01-23 09:35:31 +0100

    Add TitleLogo to header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,18 @@
+import { TitleLogo } from "../TitleLogo/TitleLogo";
 import "./NavBar.scss";
 
 export const NavBar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <header>
+      <div className="header-container">
+        <div className="header-display-desktop" data-content-field="site-title">
+          <TitleLogo />
+        </div>
+        {/* <div
+          className="header-display-mobile"
+          data-content-field="site-title"
+        ></div> */}
+      </div>
+    </header>
+  );
 };
