21309fab4cfbb24f1dd6a0824441ac6d6c1d6fab
Pernilla Sterner <psterner@hotmail.com>
2024-02-07 16:47:29 +0100

    Add hero video and adjust content to the center

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -1,14 +1,33 @@
 import "./Hero.scss";
 import heroImage from "/hero-molecular.jpg";
+import heroVideo from "/hero.mp4";
 
 export const Hero = () => {
   return (
-    <section
-      className="hero-container"
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${heroImage})`,
-      }}
-    >
+    // <section
+    //   className="hero-container"
+    //   style={{
+    //     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${heroImage})`,
+    //   }}
+    // >
+    //   <div className="hero-content">
+    //     <h1 className="hero-title">Välbefinnande och balans</h1>
+    //     <p className="hero-text">
+    //       För mig innebär bra hud- och kroppsvård en bit på vägen till
+    //       välbefinnande och att känna sig i balans och lycklig med sig själv.
+    //     </p>
+    //     <button className="hero-btn">boka en tid</button>
+    //   </div>
+    // </section>
+
+    <section className="hero-container">
+      <video
+        className="hero-video"
+        src={heroVideo}
+        autoPlay
+        loop
+        playsinline
+      ></video>
       <div className="hero-content">
         <h1 className="hero-title">Välbefinnande och balans</h1>
         <p className="hero-text">
