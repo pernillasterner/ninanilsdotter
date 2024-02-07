28a46e430692a080a51f8a8912e83705bfbf36b2
Pernilla Sterner <psterner@hotmail.com>
2024-02-07 18:38:39 +0100

    Comment out sections for now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import { About } from "../components/Sections/About/About";
 import { Hero } from "../components/Sections/Hero";
 import { Product } from "../components/Sections/Product/Product";
+import { Treatment } from "../components/Sections/Treatment/Treatment";
 import "./Home.scss";
 
 export const Home = () => {
   return (
     <main>
       <Hero />
-      <About />
+      {/* <About />
       <Product />
+      <Treatment /> */}
     </main>
   );
 };
