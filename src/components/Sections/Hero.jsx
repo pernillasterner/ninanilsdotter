6d42a059e51e691e981d6b48a5c861e7d3e7ba17
Pernilla Sterner <psterner@hotmail.com>
2024-02-07 17:51:24 +0100

    Clean up

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -4,22 +4,6 @@ import heroVideo from "/hero.mp4";
 
 export const Hero = () => {
   return (
-    // <section
-    //   className="hero-container"
-    //   style={{
-    //     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(${heroImage})`,
-    //   }}
-    // >
-    //   <div className="hero-content">
-    //     <h1 className="hero-title">Välbefinnande och balans</h1>
-    //     <p className="hero-text">
-    //       För mig innebär bra hud- och kroppsvård en bit på vägen till
-    //       välbefinnande och att känna sig i balans och lycklig med sig själv.
-    //     </p>
-    //     <button className="hero-btn">boka en tid</button>
-    //   </div>
-    // </section>
-
     <section className="hero-container">
       <video className="hero-video" src={heroVideo} autoPlay loop playsInline>
         <source src={heroVideo} type="video/mp4"></source>
