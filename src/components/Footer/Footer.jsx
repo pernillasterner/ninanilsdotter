b759270b83cf10dcdcf711febaed25adb8bbbcb3
Pernilla Sterner <psterner@hotmail.com>
2024-02-07 21:09:48 +0100

    Update address information

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,8 +14,8 @@ export const Footer = () => {
           </div>
           <div className="adress-info">
             {/* <p className="mail">info@ninanilsdotter.se</p> */}
-            <h3 className="adress">Luntmakargatan 70, 111 51 Stockholm</h3>
-            <h3 className="telefon">08 - 540 88 88</h3>
+            <h3 className="adress">Luntmakargatan 70, 113 51 Stockholm</h3>
+            <h3 className="telefon">070 - 881 98 18</h3>
             <p>
               Nina Nilsdotter | All rights reserved. Site build and designed by{" "}
               <a href="#" target="_blank">
