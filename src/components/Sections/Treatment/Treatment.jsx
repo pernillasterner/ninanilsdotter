7b04d32d81ed7001b3a1d1c9837dabcf52eaa44f
Pernilla Sterner <psterner@hotmail.com>
2024-02-08 14:50:31 +0100

    Add treatment text

--- src/components/Sections/Treatment/Treatment.jsx
+++ src/components/Sections/Treatment/Treatment.jsx
@@ -1,5 +1,17 @@
 import "./Treatment.scss";
 
 export const Treatment = () => {
-  return <section className="treatment-container">Treatment</section>;
+  return (
+    <section className="treatment-container">
+      <div className="text-banner">
+        <p>
+          Jag använder inga färdiga behandlingskoncept utan utgår istället helt
+          från dina individuella behov och önskemål. Varje behandling
+          skräddarsys för att säkerställa att du får den bästa och mest
+          personliga upplevelsen, anpassad efter just dina förutsättningar och
+          önskemål.
+        </p>
+      </div>
+    </section>
+  );
 };
