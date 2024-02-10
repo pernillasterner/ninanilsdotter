8cfbe855576598991fb073c6cdcdca5bb97d5451
Pernilla Sterner <psterner@hotmail.com>
2024-02-10 21:37:38 +0100

    Change about image and text

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -1,5 +1,5 @@
 import "./About.scss";
-import aboutNinaImage from "/nina_nilsdotter_w800h1132-724x1024.jpg";
+import aboutNinaImage from "/nina_point_w800-752x1024.jpg";
 import signatureImage from "/nina_nilsdotter_signature_small.png";
 import { AboutBanner } from "./AboutBanner";
 
@@ -10,11 +10,13 @@ export const About = () => {
         <img src={aboutNinaImage} alt="Nina Nilsdotter" />
       </div>
       <div className="about-content">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+        <h2>Det är du som står i fokus!</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit, sed do eiusmod tempor.
+          Ibland måste man även stanna upp för att låta både kropp och själ
+          återhämta sig. Min ambition är att Du skall uppnå denna balans och
+          harmoni. Jag arbetar med en helhetssyn, detta innebär att
+          behandlingar, produkter och information om hur man på bästa sätt
+          vårdar sig hemma skall samspela för att uppnå bästa resultat.
         </p>
         <div className="signature">
           <img src={signatureImage} alt="Signature" width={553} height={257} />
