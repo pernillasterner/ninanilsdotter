253649fe8b814702213eda5df336973127646e98
Pernilla Sterner <psterner@hotmail.com>
2024-02-17 22:28:26 +0100

    Testing new logo for mobile menu and change bg-color when scrolling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Header.scss";
 import { TitleLogo } from "./TitleLogo/TitleLogo";
 import { NavBar } from "./NavBar/NavBar";
@@ -6,40 +6,60 @@ import { Burger } from "./Burger/Burger";
 
 export const Header = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const heroRef = useRef(null);
 
   const handleBurgerClick = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const heroHeight = heroRef.current.clientHeight;
+      const threshold = heroHeight * 9; // Adjust the multiplier based on your needs
+
+      setIsScrolled(scrollPosition > threshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <div className="header-wrapper">
-        <div className="header-inner">
-          {/* Menu Desktop */}
-          <div
-            className="header-display-desktop"
-            data-content-field="site-title"
-          >
-            <div className="header-title-nav-wrapper">
-              {/* Title and Navigation */}
-              <TitleLogo />
-              <NavBar />
+    <header className={`header ${isScrolled ? "scrolled" : ""}`}>
+      <div ref={heroRef} className="hero">
+        <div className="header-wrapper">
+          <div className="header-inner">
+            {/* Menu Desktop */}
+            <div
+              className="header-display-desktop"
+              data-content-field="site-title"
+            >
+              <div className="header-title-nav-wrapper">
+                {/* Title and Navigation */}
+                <TitleLogo isScrolled={isScrolled} />
+                <NavBar />
+              </div>
             </div>
-          </div>
-          {/* Menu Mobile */}
-          <div
-            className="header-display-mobile"
-            data-content-field="site-title"
-          >
-            <div className="header-title-nav-wrapper">
-              {/* Title and Navigation */}
-              <TitleLogo />
+            {/* Menu Mobile */}
+            <div
+              className="header-display-mobile"
+              data-content-field="site-title"
+            >
+              <div className="header-title-nav-wrapper">
+                {/* Title and Navigation */}
+                <TitleLogo isScrolled={isScrolled} />
+              </div>
+              <Burger isOpen={isMobileMenuOpen} onClick={handleBurgerClick} />
             </div>
-            <Burger isOpen={isMobileMenuOpen} onClick={handleBurgerClick} />
           </div>
         </div>
+        {/* (Mobile Menu Navigation) */}
       </div>
-      {/* (Mobile Menu Navigation) */}
     </header>
   );
 };
--- src/components/Header/TitleLogo/TitleLogo.jsx
+++ src/components/Header/TitleLogo/TitleLogo.jsx
@@ -1,13 +1,18 @@
 import "./TitleLogo.scss";
 import headerLogo from "/logo.svg";
+import scrolledLogo from "/logo-dark.svg";
 
-export const TitleLogo = () => {
+export const TitleLogo = ({ isScrolled }) => {
+  console.log(isScrolled);
   return (
-    <div className="header-title">
+    <div className={`header-title ${isScrolled ? "scrolled" : ""}`}>
       {/* Title */}
       <div className="header-title-logo">
         <a href="/">
-          <img src={headerLogo} alt="Nina Nilsdotter" />
+          <img
+            src={isScrolled ? scrolledLogo : headerLogo}
+            alt="Nina Nilsdotter"
+          />
           {/* ninanilsdotter */}
         </a>
       </div>
